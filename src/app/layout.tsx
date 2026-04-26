eb88693fcbec2323c154d7ae9d11cbee5fed54e2
Abhishek Saini <abhisheksaini80597@gmail.com>
2026-04-26 13:15:10 +0530

    add logo in seo recm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,11 +35,20 @@ export const metadata: Metadata = {
     siteName: companyName,
     locale: "en_US",
     type: "website",
+    images: [
+      {
+        url: `${defaultSiteUrl}/logo.jpeg`,
+        width: 1200,
+        height: 630,
+        alt: companyName,
+      },
+    ],
   },
   twitter: {
     card: "summary_large_image",
     title: `${companyName} | Job Consulting & Placement`,
     description: companyTagline,
+    images: [`${defaultSiteUrl}/logo.jpeg`],
   },
 };
 
